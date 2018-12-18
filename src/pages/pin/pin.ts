0bd6a7956f186dffa7d81a5f29add387539fb672
Jair Huaman Bellido <huamanbellidoj@gmail.com>
2018-12-18 11:04:37 -0500

    Se realizo el autofocus del PIN al iniciar la aplicacion

--- src/pages/pin/pin.ts
+++ src/pages/pin/pin.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component,ViewChild,ElementRef } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
 import { HomePage } from "../home/home";
 import { PinDAO } from "../../source/daos/PinDAO";
@@ -9,6 +9,14 @@ import { PinDAO } from "../../source/daos/PinDAO";
 })
 export class PinPage {
 	private pin: string = "";
+
+	@ViewChild('pinref1') pinInput1:ElementRef;
+	@ViewChild('pinref2') pinInput2:ElementRef;
+	@ViewChild('pinref3') pinInput3:ElementRef;
+	@ViewChild('pinref4') pinInput4:ElementRef;
+	@ViewChild('pinref5') pinInput5:ElementRef;
+	@ViewChild('pinref6') pinInput6:ElementRef;
+
 
 	constructor(
 		public navCtrl: NavController,
@@ -22,5 +30,39 @@ export class PinPage {
 
 		// Se redirige al home de la aplicación
 		this.navCtrl.setRoot(HomePage);
+
+
+		this.pinInput1.nativeElement.focus();
+	}
+
+	private focus2(ev) {
+		if(ev.target.value.length ===1 ){
+			this.pinInput2.nativeElement.focus();
+		}
+
+	}
+	private focus3(ev) {
+		if(ev.target.value.length ===1 ){
+			this.pinInput3.nativeElement.focus();
+		}
+
+	}
+	private focus4(ev) {
+		if(ev.target.value.length ===1 ){
+			this.pinInput4.nativeElement.focus();
+		}
+
+	}
+	private focus5(ev) {
+		if(ev.target.value.length ===1 ){
+			this.pinInput5.nativeElement.focus();
+		}
+
+	}
+	private focus6(ev) {
+		if(ev.target.value.length ===1 ){
+			this.pinInput6.nativeElement.focus();
+		}
+
 	}
 }
