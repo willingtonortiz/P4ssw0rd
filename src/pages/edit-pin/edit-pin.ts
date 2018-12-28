1e96b769ac43ee7eb45f3109063c7354f3c09244
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2018-12-27 23:57:40 -0500

    Se agregó el pinVerifier en la página de editarPin, falta agregar estilos

--- src/pages/edit-pin/edit-pin.ts
+++ src/pages/edit-pin/edit-pin.ts
@@ -1,18 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { NavController, LoadingController, Loading } from "ionic-angular";
 import { EncryptorAccountProvider } from "../../providers/encryptor-account/encryptor-account";
 import { PinDAO } from "../../source/daos/PinDAO";
 import { AccountClassifier } from "../../providers/AccountClassifier";
+import { VerifyPinComponent } from "../../components/verify-pin/verify-pin";
 
 @Component({
 	selector: "page-edit-pin",
 	templateUrl: "edit-pin.html"
 })
 export class EditPinPage {
+	private headerText: string = "Ingrese su pin";
 	private verified: boolean = false;
-	private pin: string = "";
-	private inputText: string = "Ingrese su pin";
-	private buttonText: string = "Continuar";
+	private waitForPinConfirmation: boolean = false;
+	@ViewChild(VerifyPinComponent) private verifyPin: VerifyPinComponent;
 
 	constructor(
 		private navCtrl: NavController,
@@ -22,38 +23,64 @@ export class EditPinPage {
 		private accountClassifier: AccountClassifier
 	) {}
 
-	private editPin(): void {
-		if (!this.verified) {
-			this.pinDao.verifyPin(this.pin).then((data: boolean) => {
-				if (data) {
-					this.inputText = "Ingrese el nuevo pin";
-					this.buttonText = "Guardar nuevo pin";
-					this.pin = "";
-					this.verified = true;
-				}
-			});
-		} else {
-			// Se modifica el pin
-			this.pinDao.setPin(this.pin);
+	private onPinWritten(pin: string): void {
+		// Si se escribió un pin correcto
+		if (pin !== null) {
+			// Si el pin no está verificado
+			if (!this.verified) {
+				// Si es el pin correcto
+				this.pinDao.verifyPin(pin).then((data: boolean) => {
+					// Animación de confirmación de pin
+					this.waitForPinConfirmation = true;
+					setTimeout(() => {
+						// Quitar animación de pin
+						this.waitForPinConfirmation = false;
+					}, 1500);
 
-			// Actualizar todas las cuentas y la base de datos
-			this.encryptorAccountProvider.modifyAccounts(this.pin).then(() => {
-				// Actualizar el arreglo actual de cuentas
-				this.accountClassifier.getAccounts();
-			});
+					if (data) {
+						// Está verificado
+						this.verified = true;
+						this.verifyPin.cleanInputs();
+						this.headerText = "Ingrese el nuevo pin";
+					}
+				});
+			} else {
+				// Se modifica el pin
+				this.pinDao.setPin(pin);
 
-			// Animación de modificación de pin
-			const loading: Loading = this.loadingController.create({
-				content: "Modificando pin",
-				duration: 1500
-			});
-			loading.present();
+				// Actualizar todas las cuentas y la base de datos
+				this.encryptorAccountProvider.modifyAccounts(pin).then(() => {
+					// Actualizar el arreglo actual de cuentas
+					this.accountClassifier.getAccounts();
+				});
 
-			// Cuando termina la carga
-			loading.onDidDismiss(() => {
-				// Seguir con la navegación
-				this.navCtrl.pop();
-			});
+				// Animación de modificación de pin
+				const loading: Loading = this.loadingController.create({
+					content: "Modificando pin",
+					duration: 1500
+				});
+				loading.present();
+
+				// Cuando termina la carga
+				loading.onDidDismiss(() => {
+					// Seguir con la navegación
+					this.navCtrl.pop();
+				});
+			}
 		}
 	}
+
+	// private editPin(): void {
+	// 	if (!this.verified) {
+	// 		this.pinDao.verifyPin(this.pin).then((data: boolean) => {
+	// 			if (data) {
+	// 				this.inputText = "Ingrese el nuevo pin";
+	// 				this.buttonText = "Guardar nuevo pin";
+	// 				this.pin = "";
+	// 				this.verified = true;
+	// 			}
+	// 		});
+	// 	} else {
+	// 	}
+	// }
 }
